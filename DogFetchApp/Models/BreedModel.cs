b1244f1af4b033e9a85801f7e019f56b3100ebab
jftremblay86 <58572712+jftremblay86@users.noreply.github.com>
2020-10-07 00:53:15 -0400

    Add files via upload

---
+++ DogFetchApp/Models/BreedModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogFetchApp.Models
+{
+
+    public class BreedModel
+    {
+        [JsonProperty("message")] public Dictionary<string, List<string>> Breed { get; set; }
+    }
+}
